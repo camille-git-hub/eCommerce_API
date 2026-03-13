5941af76ed481a7ad14934cc4023c02b7a53ed30
« Camille Rahout» <camille.rahout@gmail.com>
2026-03-13 16:51:48 +0100

    added swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import swaggerUi from "swagger-ui-express";
+import openApiSpec from "./docs/openapi.json" with { type: "json" };
 import express from 'express';
 import '../src/db/index.ts';
 import { userRoutes, productRoutes, categoryRoutes, orderRoutes } from './routes/index.ts';
@@ -5,10 +7,12 @@ import { errorHandler, notFoundHandler } from '#middleware';
 import {validateBody} from '#middleware';
 import { orderInputSchema, productSchema, categorySchema, newCategorySchema } from '#schemas';
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiSpec));
 
 app.use('/users', userRoutes);
 app.use('/products', validateBody(productSchema), productRoutes);
